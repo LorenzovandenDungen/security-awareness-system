c1b77422cbfa330f518f4ebd00942c0f77a8c78c
LorenzovandenDungen <2130766@talnet.nl>
2023-06-12 23:46:21 +0200

    save

---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here you would typically handle the form submission, for example, send a request to your backend
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Log in</button>
+    </form>
+  );
+}
+
+export default Login;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomePage() {
+  return <h1>Welcome to the Security Awareness System</h1>;
+}
+
+export default HomePage;
