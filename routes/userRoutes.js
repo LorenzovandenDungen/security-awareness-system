4a47dceb018bdf7b351f03f0fcfb0be38e2239dc
LorenzovandenDungen <2130766@talnet.nl>
2023-06-21 13:08:15 +0200

    save

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const User = require('../client/src/models/User');
 const router = express.Router();
 
-// Define your user related routes here
-// For example:
-router.get('/', (req, res) => {
-  // Fetch users from database and send response
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const user = new User({ name, email, password, role });
+    // Don't forget to hash the password before saving!
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to register user' });
+  }
 });
 
 module.exports = router;
